f85697505c01a00d9afa05fd22b1d4aa8a483514
Matthew Hatcher <matthewh@halfpetal.com>
2020-04-29 13:56:13 -0400

    Changing to handle new login auth.

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,20 +2,39 @@
 
 namespace App\Http\Middleware;
 
+use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Closure;
 
-class Authenticate extends Middleware
+class Authenticate
 {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if (!$request->session()->has('ttv_user')) {
+            return redirect()->route('login');
+        }
+
+        return $next($request);
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
-    protected function redirectTo($request)
+    /*protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
             return route('login');
         }
-    }
+    }*/
 }
