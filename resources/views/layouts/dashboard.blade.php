9f5cff1fd10522c596a4054bfdcf24d170f98025
Matthew Hatcher <matthewh@halfpetal.com>
2020-05-01 11:17:56 -0400

    Adding mix.

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -3,9 +3,11 @@
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ (isset($pageTitle) ? $pageTitle . ' / ' : '') . config('app.name', 'Laravel') }}</title>
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+    <script src="{{ mix('/js/app.js') }}"></script>
 </head>
 <body>
-    <div>
+    <div class="container mx-auto pt-4">
         @yield('content')
     </div>
 </body>
