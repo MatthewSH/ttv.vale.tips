a39b1dfdd97840cc7d3e3618104f02733732e448
Matthew Hatcher <matthewh@halfpetal.com>
2020-05-01 15:30:57 -0400

    Adding the the Twitch user service injection.

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,3 +1,5 @@
+@inject('user', 'App\Services\TwitchUser')
+
 <div>
     <!-- Act only according to that maxim whereby you can, at the same time, will that it should become a universal law. - Immanuel Kant -->
 </div>
