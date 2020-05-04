31c12f9ec71345d862f80ce4ca3bffcc22f47e50
Matthew Hatcher <matthewh@halfpetal.com>
2020-05-04 11:05:10 -0400

    Returning the views and data.

--- app/Http/Controllers/Dashboard/CategoryController.php
+++ app/Http/Controllers/Dashboard/CategoryController.php
@@ -11,21 +11,23 @@ class CategoryController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function index()
     {
-        //
+        return view('dashboard.categories.index', [
+            'categories' => app('rinvex.categories.category')->all(),
+        ]);
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function create()
     {
-        //
+        return view('dashboard.categories.create');
     }
 
     /**
