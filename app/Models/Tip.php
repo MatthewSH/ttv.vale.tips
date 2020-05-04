7b58c7c8320bc51062cae914995eae1b3c579710
Matthew Hatcher <matthewh@halfpetal.com>
2020-05-04 11:02:27 -0400

    Adding the Tip model.

---
+++ app/Models/Tip.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Tip extends Model
+{
+    use \Rinvex\Categories\Traits\Categorizable, SoftDeletes;
+
+    protected $fillable = [
+        'user_id', 'title', 'short',
+        'long'
+    ];
+}
---
+++ database/migrations/2020_05_04_124644_create_tips_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tips', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->text('short')->nullable()->default(null);
+            $table->longText('long');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tips');
+    }
+}
