2c8a6261db9f021644a6e0d010648461ad701949
Matthew Hatcher <matthewh@halfpetal.com>
2020-05-04 11:51:53 -0400

    First parts to the index

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.dashboard')
 
 @section('content')
+<div class="mb-8 flex">
+    <a href="{{ route('dashboard.categories.create') }}" class="btn btn-green">New Category</a>
+</div>
 @endsection
