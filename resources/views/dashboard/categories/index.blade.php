8c2b18004018fe12e483c784bbdda022d6ec093e
Matthew Hatcher <matthewh@halfpetal.com>
2020-05-04 12:29:16 -0400

    Showing the list of categories.

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -4,4 +4,22 @@
 <div class="mb-8 flex">
     <a href="{{ route('dashboard.categories.create') }}" class="btn btn-green">New Category</a>
 </div>
+
+<div class="flex">
+    @forelse($categories as $category)
+    <div class="flex-1 bg-white shadow-lg p-6 mx-3 w-auto rounded">
+        <a href="{{ route('dashboard.categories.show', ['category' => $category->id]) }}" class="text-3xl hover:text-blue-600">{{ $category->name }}</a>
+        <p class="italic text-gray-600 ml-4 mb-6">
+            {{ $category->descendants()->count() }} Tips
+        </p>
+        <a href="#" class="btn-sm btn-blue">New Tip</a>
+    </div>
+
+    @if(!$loop->last)
+        <hr />
+    @endif
+    @empty
+    <p>No categories...yet.</p>
+    @endforelse
+</div>
 @endsection
