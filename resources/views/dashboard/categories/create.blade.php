7508b4d118fdc4f861be62948e5416712eef9d7e
Matthew Hatcher <matthewh@halfpetal.com>
2020-05-04 11:50:54 -0400

    Creating the create form.

--- resources/views/dashboard/categories/create.blade.php
+++ resources/views/dashboard/categories/create.blade.php
@@ -1,4 +1,23 @@
 @extends('layouts.dashboard')
 
 @section('content')
+<form class="w-full max-w-lg" method="POST" action="{{ route('dashboard.categories.store') }}">
+    {{ method_field('POST') }}
+    {{ csrf_field() }}
+    <div class="flex flex-wrap -mx-3 mb-4">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                Category Name
+            </label>
+            <input name="name" type="text" placeholder="Overwatch" class="appearance-none block w-full bg-gray-100 text-gray-700 border {{ $errors->has('name') ? 'border-red-500' : 'border-gray-500 ' }} rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white">
+            @if($errors->has('name'))
+                @foreach($errors->get('name') as $message)
+                    <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                @endforeach
+            @endif
+        </div>
+    </div>
+
+    <button type="submit" class="btn-sm btn-blue">Create Category</button>
+</form>
 @endsection
