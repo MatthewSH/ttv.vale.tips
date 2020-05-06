3013f86c519252fd3169ded82911eb79b95d7dbf
Matthew Hatcher <matthewh@halfpetal.com>
2020-05-06 10:47:49 -0400

    Adding display of sub categories.

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -9,8 +9,19 @@
     @forelse($categories as $category)
     <div class="bg-white shadow-md hover:shadow-xl p-6 mx-3 w-auto rounded">
         <a href="{{ route('dashboard.categories.show', ['category' => $category->id]) }}" class="text-3xl hover:text-blue-600">{{ $category->name }}</a>
+        @if($category->descendants()->count() > 0)
+        <div class="ml-1 text-gray-800">
+            <p class="text-lg">Sub-categories</p>
+            <div class="ml-3 mt-2 grid grid-cols-4 gap-1">
+                @foreach(Illuminate\Support\Arr::sort($category->descendants, function($c) { return $c->name; }) as $child)
+                    <a href="{{ route('dashboard.categories.show', ['category' => $child->id]) }}" class="font-semibold hover:text-blue-600">{{ $child->name }}</a>
+                @endforeach
+            </div>
+        </div>
+        <hr class="my-3"/>
+        @endif
         <p class="italic text-gray-600 ml-4 mb-6">
-            {{ $category->descendants()->count() }} Tips
+            {{ $category->descendants()->count() }} Tips {{-- TODO: Fix this. This is the WRONG count() --}}
         </p>
         <a href="#" class="btn-sm btn-blue">New Tip</a>
     </div>
