7d36f57891f3b0021069739826265dc89f7a3024
Matthew Hatcher <matthewh@halfpetal.com>
2020-05-06 13:13:13 -0400

    Adding createtip request.

---
+++ app/Http/Requests/CreateTip.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+
+class CreateTip extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @param Request $request
+     * @return bool
+     */
+    public function authorize(Request $request)
+    {
+        return ($request->session()->has('ttv_user') && $request->session()->get('ttv_user')->is_author);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|min:1',
+            'short' => 'sometimes',
+            'long' => 'required|min:1',
+            'visible' => 'sometimes|boolean'
+        ];
+    }
+}
