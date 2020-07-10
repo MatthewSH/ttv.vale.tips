99fa7f7d37a7d481ad2740e6e6a2cc005692070a
Matthew Hatcher <matthewh@halfpetal.com>
2020-07-10 18:59:45 -0400

    Remove id from the response.

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -15,7 +15,7 @@ class CategoryResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
+            //'id' => $this->id,
             'slug' => $this->slug,
             'name' => $this->name,
             'description' => $this->description,
